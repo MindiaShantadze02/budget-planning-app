eb6f9fccd29ea45fcf28d2f0dfc654e40c5168c2
Mindia Shantadze <shantadze200202@gmail.com>
2022-03-05 16:39:12 +0400

    created user schema&model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+// defining schema
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        maxlength: 255,
+        minlength: 7,
+        required: [true, 'Please add your email']
+    },
+    password: {
+        type: String,
+        maxlength: 255,
+        minlength: 10,
+        required: [true, 'Please enter your password']
+    },
+    role: String,
+    firstName: {
+        type: String,
+        required: [true, 'Please enter your firstname'],
+        maxlength: 255,
+        minlength: 2
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Please enter your lastname'],
+        maxlength: 255,
+        minlength: 2
+    },
+    gender: {
+        required: [true, 'Please enter your gender'],
+        type: String,
+        enum: {
+            values: ['Male', 'Female', 'Other'],
+        }
+    },
+    birthDate: {
+        type: Date,
+        required: [true, 'Please enter your birthday']
+    },
+    country: {
+        type: 'String',
+        required: [true, 'Please enter country of residence']
+    }
+});
+
+// defining and exporting user schema
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
